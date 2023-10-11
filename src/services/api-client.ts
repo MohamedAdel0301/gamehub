363aa0c91ad6bde9449c909f30b602cc3d748470
MohamedAdel0301 <99405693+MohamedAdel0301@users.noreply.github.com>
2023-10-11 04:42:05 +0300

    created generic api client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "5362348718ba4f8d9ee1c4244b7a91a8",
@@ -11,3 +11,17 @@ export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
+
+class APIClient<T> {
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+  getAll = (config?: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
