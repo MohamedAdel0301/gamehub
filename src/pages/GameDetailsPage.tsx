06c7e0445ea50147c77bb0ee9e23a9d73621ea3c
MohamedAdel0301 <99405693+MohamedAdel0301@users.noreply.github.com>
2023-10-12 01:00:07 +0300

    Handle errors

---
+++ src/pages/GameDetailsPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GameDetailsPage() {
+  return <div>Details</div>;
+}
+
+export default GameDetailsPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import GameDetailsPage from "./pages/GameDetailsPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<Layout/>,
-        children:[{
-            index:true,element:<HomePage/>
-        }]
-    }
-])
- 
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "games/:id", element: <GameDetailsPage /> },
+    ],
+  },
+]);
+
 export default router;
