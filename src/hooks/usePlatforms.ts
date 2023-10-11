74f8a60aeb285d00e62013679e1885a56c0b03cd
MohamedAdel0301 <99405693+MohamedAdel0301@users.noreply.github.com>
2023-10-11 04:23:13 +0300

    change exports

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,12 +2,7 @@ import platforms from "../data/platforms";
 import apiClient from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
 import { FetchResponse } from "../services/api-client";
-
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import { Platform } from "./useGames";
 
 const usePlatforms = () =>
   useQuery({
